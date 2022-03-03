4f526f97743a2c19ff2e80d62111374c20f8889e
Robin Wollenschlaeger <r.wollenschlaeger@goettfert.de>
2022-03-03 15:57:26 +0100

    format

--- src/index.ts
+++ src/index.ts
@@ -6,52 +6,51 @@ import path from "path";
 import 'dotenv/config';
 import { Server } from 'socket.io';
 
-(
-    async function () {
-        try {
+(async function () {
+    try {
 
-            /**************************
-            Web server Configuration
-            **************************/
-            const app = express();
-            const server = http.createServer(app);
+        /**************************
+        Web server Configuration
+        **************************/
+        const app = express();
+        const server = http.createServer(app);
 
-            //Here we are configuring express to use body-parser as middle-ware.
-            app.use(bodyParser.urlencoded({
-                extended: true
-            }));
-            app.use(bodyParser.json({
-            }));
+        //Here we are configuring express to use body-parser as middle-ware.
+        app.use(bodyParser.urlencoded({
+            extended: true
+        }));
+        app.use(bodyParser.json({
+        }));
 
-            /**************************
-            REST-Api Hooks
-            **************************/
+        /**************************
+        REST-Api Hooks
+        **************************/
 
-            /**
-            * initial webservice
-            * send startpage
-            */
-            app.get('/', async function (request, response) {
-                try {
-                    return response.sendFile(path.resolve(__dirname + "/frontend/index.html"));
-                } catch (e) {
-                    console.error(e);
-                }
-            });
+        /**
+        * initial hook
+        * send startpage
+        */
+        app.get('/', async function (request, response) {
+            try {
+                return response.sendFile(path.resolve(__dirname + "/frontend/index.html"));
+            } catch (e) {
+                console.error(e);
+            }
+        });
 
 
-            app.use(express.static(path.normalize(__dirname + "/frontend/")));
-            const io = new Server(server);
-            io.on("connection", (socket)=>{
-                    console.log("Socket connected");
-            });
-            server.listen(process.env.SERVERPORT, async function () {
-                try {
-                    console.log(`started && running @ port ${process.env.SERVERPORT}`);
+        app.use(express.static(path.normalize(__dirname + "/frontend/")));
+        const io = new Server(server);
+        io.on("connection", (socket) => {
+            console.log("Socket connected");
+        });
+        server.listen(process.env.SERVERPORT, async function () {
+            try {
+                console.log(`started && running @ port ${process.env.SERVERPORT}`);
 
-                } catch (e) { console.error(e) }
-            });
-        } catch (e) {
-            console.error(e);
-        }
-    })()
+            } catch (e) { console.error(e) }
+        });
+    } catch (e) {
+        console.error(e);
+    }
+})()
